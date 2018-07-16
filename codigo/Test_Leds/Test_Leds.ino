c1631545ec66c4d1d062ce1701ea6e214796c8c3
javacasm <javacasm@gmail.com>
2018-07-16 10:02:44 +0200

    Update

--- codigo/Test_Leds/Test_Leds.ino
+++ codigo/Test_Leds/Test_Leds.ino
@@ -1,19 +1,21 @@
-void setup()
-{
-  pinMode(16, OUTPUT);
-pinMode(2, OUTPUT);
+
+int espera = 1000; // Tiempo es parpadeo
+
+
+void setup() {
+  pinMode(D4, OUTPUT); // Configuramos las patillas D0 y D4 como salida
+  pinMode(D0, OUTPUT);
 
 }
 
 
-void loop()
-{
+void loop() {
 
-    digitalWrite(16,HIGH);
-    digitalWrite(2,LOW);
-    delay(1000);
-    digitalWrite(16,LOW);
-    digitalWrite(2,HIGH);
-    delay(1000);
+    digitalWrite(D4,HIGH);
+    digitalWrite(D0,LOW);
+    delay(espera); // Esperamos un tiempo para poder apreciar el parpadeo
+    digitalWrite(D4,LOW);
+    digitalWrite(D0,HIGH);
+    delay(espera);
 
 }
