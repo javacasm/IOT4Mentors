d3d02fc8a61dcc428e30675dba3d8490fc86c229
Javacasm <javacasm@gmail.com>
2018-12-12 14:05:40 +0100

    add

---
+++ codigo/Riego_automatica_Rele/Riego_automatica_Rele.ino
@@ -0,0 +1,20 @@
+#define Sensor_Humedad A0
+#define Rele_1 D4
+
+int iUmbralHumedad = 500;
+
+void setup()  {
+  pinMode(Rele_1, OUTPUT);     // Activamos la salida del rele
+}
+
+
+void loop() {
+   int valorHumedad = analogRead(Sensor_LDR);
+   
+   if( valorLDR <= iUmbralHumedad) {  // Si el valor de humedad es menor que el umbral se activa el rele 
+    digitalWrite(Rele_1,HIGH);
+   } else {
+    digitalWrite(Rele_1,LOW);
+   }
+
+}
